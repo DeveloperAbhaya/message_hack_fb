f8636d3479da8c48dfcb9bd9d1dc3c385a662129
DeveloperAbhaya <159024823+DeveloperAbhaya@users.noreply.github.com>
2024-05-18 20:35:34 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+while True:
+    pyautogui.typewrite('hanne k ma')
+    pyautogui.press('enter')
+    time.sleep(0.2)  # Adding a sleep to avoid overwhelming the system with keypressespheri ruk yo hack hopheri ruk yo hack ho
